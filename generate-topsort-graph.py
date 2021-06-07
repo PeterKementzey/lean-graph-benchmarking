c422fa741f03cc8378dec14eb103d4a5352c0753
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-07 04:05:08 +0200

    Add node count to edge list files for simplicity

--- generate-topsort-graph.py
+++ generate-topsort-graph.py
@@ -3,17 +3,11 @@
 import random, sys
 
 def generateDAG (nodeCount, edgeCount, fileName):
-    # nodeCount = 20
-    # edgeCount = 200
-
     nodes = list(range(nodeCount))
 
     random.shuffle(nodes) # create a random order for the nodes, this will be one possible result for the topological sorting and this will guarantee that there are no cycles
 
-    # print(nodes)
-
-    def getPlaceInOrder(x): return nodes[x]
-
+    def getPosition(x): return nodes[x]
     def swapTuple(t): return (t[1], t[0])
 
     edges = []
@@ -22,21 +16,21 @@ def generateDAG (nodeCount, edgeCount, fileName):
         newPair = (random.randint(0,nodeCount-1), random.randint(0,nodeCount-1))
         while newPair[0] == newPair[1]:
             newPair = (random.randint(0,nodeCount-1), random.randint(0,nodeCount-1))
-        orderedPair = newPair if getPlaceInOrder(newPair[0]) <= getPlaceInOrder(newPair[1]) else swapTuple(newPair)
+        orderedPair = newPair if getPosition(newPair[0]) <= getPosition(newPair[1]) else swapTuple(newPair)
         edges.append(orderedPair)
 
-    sys.stdout = open(fileName, 'w')
-
+    sys.stdout = open(fileName + "-topsort-gen.txt", 'w')
+    print(nodeCount)
     for pair in edges:
         print (str(pair[0]) + " " + str(pair[1]))
-    
     sys.stdout.close()
-    
-    sys.stdout = open(fileName + "-original-sequence.txt", 'w')
+
+    sys.stdout = open(fileName + "-topsorted-sequence.txt", 'w')
     sortedOrder = list(enumerate(nodes)); sortedOrder.sort(key = lambda x : x[1]); sortedOrder = list(map(lambda x : x[0], sortedOrder))
     print(sortedOrder)
     sys.stdout.close()
 
 
 
-generateDAG(20, 10000, "topsort-gen.txt")
+# bitcoinSize = generateDAG(5881, 35592, "stanford-bitcoin-sized")
+maximumSize = generateDAG(6600, 450000, "maximum-working-size") # these values seem to be close to the max the system can handle with crashes not happening too often
