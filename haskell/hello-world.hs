07c97a61e580e0c4eda9a0b07feb891bcdc36ecf
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-07 18:28:37 +0200

    Implement graph parsing in Haskell

--- haskell/hello-world.hs
+++ haskell/hello-world.hs
@@ -25,24 +25,35 @@ exampleGraph8 :: Graph
 testGraph :: [Char]
 testGraph = "../generated-graphs/test-topsort-gen.txt"
 
-createGraph :: Vertex -> [Edge] -> Graph
-createGraph nodeCount edgeList =
+createGraph :: (Vertex, [Edge]) -> Graph
+createGraph (nodeCount, edgeList) =
     let bounds = (0, nodeCount-1) in
     buildG bounds edgeList
+
+parseEdgeList :: [String] -> [(Int, Int)]
+parseEdgeList (edge:edgeList) = let pair = words edge in (read (head pair), read (pair !! 1)) : parseEdgeList edgeList
+parseEdgeList [] = []
+
+parseLines :: [String] -> (Int, [(Int, Int)])
+parseLines (nodeCount:edgeList) = (read nodeCount, parseEdgeList edgeList)
+parseLines [] = error "Cannot parse empty file"
+
+parseFile :: FilePath -> IO Graph 
+parseFile filePath = do
+    contents <- readFile filePath
+    let linesOfFile = lines contents
+    let parsed = parseLines linesOfFile
+    return (createGraph parsed)
 
 
 -- Note: can use command line arguments I think like this: (args !! 0)
 main :: IO ()
-main =
-    let nodeCount = 10 in
-    let edgeList = [(4, 0), (9, 8), (2, 8), (5, 0), (4, 8), (5, 2), (6, 3), (1, 3)] in
-    -- let filePath = testGraph
-    -- contents <- readFile filePath
-    -- print contents
-    -- let linesOfFile = lines contents
-    -- print linesOfFile
+main = do
+    let filePath = testGraph
+
+    graph <- parseFile filePath
     
-    print (topSort (createGraph nodeCount edgeList))
+    print (topSort graph)
 
 -- main = defaultMain [
 --   bgroup "fib" [ bench "1"  $ whnf show (topSort exampleGraph8)
