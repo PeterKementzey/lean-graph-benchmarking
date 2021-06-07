b51137cf7d9428099114d8cbb56589cfd4bc0dd6
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-07 18:02:55 +0200

    Create haskell graph from node count and edge list

--- haskell/hello-world.hs
+++ haskell/hello-world.hs
@@ -25,12 +25,24 @@ exampleGraph8 :: Graph
 testGraph :: [Char]
 testGraph = "../generated-graphs/test-topsort-gen.txt"
 
+createGraph :: Vertex -> [Edge] -> Graph
+createGraph nodeCount edgeList =
+    let bounds = (0, nodeCount-1) in
+    buildG bounds edgeList
+
+
 -- Note: can use command line arguments I think like this: (args !! 0)
 main :: IO ()
-main = do
+main =
+    let nodeCount = 10 in
+    let edgeList = [(4, 0), (9, 8), (2, 8), (5, 0), (4, 8), (5, 2), (6, 3), (1, 3)] in
+    -- let filePath = testGraph
+    -- contents <- readFile filePath
+    -- print contents
+    -- let linesOfFile = lines contents
+    -- print linesOfFile
     
-    
-    print (topSort exampleGraph8)
+    print (topSort (createGraph nodeCount edgeList))
 
 -- main = defaultMain [
 --   bgroup "fib" [ bench "1"  $ whnf show (topSort exampleGraph8)
