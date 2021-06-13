ef77f9dd77016f310e997d05ad086a6a28c76288
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-13 21:18:45 +0200

    Add build script that gets dependency

---
+++ lean/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf build
+
+git clone https://github.com/PeterKementzey/graph-library-for-lean4 build/deps/Graph
+
+(cd build/deps/Graph; git checkout --detach e73f70c848de80cce8cbff50aa8586496bc47c1d; leanpkg build lib)
+
+leanpkg build bin LINK_OPTS=build/deps/Graph/build/lib/libGraph.a
