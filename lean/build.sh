1776b56a860ca5b0c863b582ed6fd2081f50b59b
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-14 10:03:36 +0200

    Update lean build system

--- lean/build.sh
+++ lean/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# rm -rf build # FIXME reenable this
+rm -rf build
 
 git clone https://github.com/PeterKementzey/graph-library-for-lean4 build/deps/Graph
 
-(cd build/deps/Graph; git checkout --detach aa0c39b12e04f63a7cc6da475c592b522876618a; leanpkg build lib)
+(cd build/deps/Graph; git checkout --detach aa0c39b12e04f63a7cc6da475c592b522876618a; leanpkg build lib) # TODO update commit hash
 
 leanpkg build bin LINK_OPTS=build/deps/Graph/build/lib/libGraph.a
