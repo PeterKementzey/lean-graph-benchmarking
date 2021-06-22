0b0eae1d9614c03ce259f77bebae59f14565bbfd
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-22 15:13:20 +0200

    Use file handles for generating graphs, remove previously generated graphs

--- generate-topsort-graph.py
+++ generate-topsort-graph.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-import random, sys
+import random, subprocess
 
-def generateDAG (nodeCount, edgeCount, fileName):
+graphDirectory = "generated-graphs/"
+
+def generateDAG (nodeCount, edgeCount, fileName, seed):
+    random.seed(seed)
+
     nodes = list(range(nodeCount))
 
     random.shuffle(nodes) # create a random order for the nodes, this will be one possible result for the topological sorting and this will guarantee that there are no cycles
@@ -19,28 +23,38 @@ def generateDAG (nodeCount, edgeCount, fileName):
         orderedPair = newPair if getPosition(newPair[0]) <= getPosition(newPair[1]) else swapTuple(newPair)
         edges.append(orderedPair)
 
-    sys.stdout = open(fileName + "-topsort-gen.txt", 'w')
-    print(nodeCount)
+    fileHandle = open(graphDirectory + fileName + ".txt", 'w')
+    print(nodeCount, file=fileHandle)
     for pair in edges:
-        print (str(pair[0]) + " " + str(pair[1]))
-    sys.stdout.close()
+        print (str(pair[0]) + " " + str(pair[1]), file=fileHandle)
+    fileHandle.close()
 
-    sys.stdout = open(fileName + "-topsorted-sequence.txt", 'w')
+    fileHandle = open(graphDirectory + fileName + "-topsorted-sequence.txt", 'w')
     sortedOrder = list(enumerate(nodes)); sortedOrder.sort(key = lambda x : x[1]); sortedOrder = list(map(lambda x : x[0], sortedOrder))
-    print(sortedOrder)
-    sys.stdout.close()
+    print(sortedOrder, file=fileHandle)
+    fileHandle.close()
 
-# TODO use seed to generate predictable graph
+graphParameters = [
+    (500, 2000, "small-sparse"),
+    (500, 225000, "small-dense"),
+    (3000, 10000, "medium-very-sparse"),
+    (3000, 500000, "medium-sparse"),
+    (3000, 8500000, "medium-dense"),
+    (3000, 12000000, "medium-very-dense"),
+    (6600, 450000, "maximum-working-size") ,
+    (30000, 2000000, "huge")
+]
 
-# bitcoinSize = generateDAG(5881, 35592, "generated-graphs/stanford-bitcoin-sized")
-# maximumSize = generateDAG(6600, 450000, "generated-graphs/maximum-working-size") # these values seem to be close to the max the system can handle with crashes not happening too often
-# mediumDense = generateDAG(3000, 8500000, "generated-graphs/medium-dense")
-# mediumVeryDense = generateDAG(3000, 12000000, "generated-graphs/medium-very-dense")
-# mediumSparse = generateDAG(3000, 500000, "generated-graphs/medium-sparse")
-# mediumVerySparse = generateDAG(3000, 10000, "generated-graphs/medium-very-sparse")
-# smallDense = generateDAG(500, 225000, "generated-graphs/small-dense")
-# smallSparse = generateDAG(500, 2000, "generated-graphs/small-sparse")
+# remove all generated graphs first
 
-# testGraph = generateDAG(10, 8, "generated-graphs/test")
+subprocess.run("rm -f" + graphDirectory + "*.txt", shell=True)
 
-hugeSize = generateDAG(30000, 2000000, "generated-graphs/huge")
+# build generated graphs
+
+# for p in graphParameters:
+#     for i in (range(10)):
+        
+
+# build lean and haskell
+
+# run tests
