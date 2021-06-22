b205c837513dfa9a65d433c7beb4b40ff3cfbfce
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-22 20:31:53 +0200

    Implement Lean function evaluate and benchmark functions

--- lean/Benchmark.lean
+++ lean/Benchmark.lean
@@ -1,7 +1,6 @@
-import Graph
-
-import Graph.ExampleGraphs
-import Graph.TraverseExample
+import Graph.Graph
+import Graph.Parser
+import Graph.TopologicalSort
 
 
 def euMails := "../stanford-graphs/email-EuAll.txt"
@@ -19,35 +18,43 @@ def smallSparse := "../generated-graphs/small-sparse-topsort-gen.txt"
 def huge := "../generated-graphs/huge-topsort-gen.txt"
 def testGraph := "../generated-graphs/test-topsort-gen.txt"
 
--- def main (argv : List String) : IO Unit := do
 
---   let filePath := huge
+/-- This function ensures that the expression does not get dropped by compiler optimizations. -/
+def evaluate [ToString α] (expression : α) : IO Unit := IO.FS.writeFile "/dev/null" (toString expression)
 
---   let graph <- parseGraphFromEdgeListFile filePath
---   let res <- graph.breadthFirstCompleteTraversalOrder5
---   -- let res <- exampleGraph3.breadthFirstCompleteTraversalOrder4
---   -- let res <- exampleLineGraph.depthFirstTraversalOrderWithLeaving4
---   -- let res <- exampleGraph3.getAllVertexIDs
---   -- let res <- graph.topSortUnsafe
---   -- let res <- graph.topSort
---   -- let res <- graph.dijkstraWithTarget 8717 27573
+def benchmarkParsing (filePath : String) : IO (Graph Bool Nat) := do
+  let initial <- IO.monoMsNow
+  let input <- IO.FS.lines filePath
+  let (nodeCount, edgeList) := parseEdgeList input
+  let start <- IO.monoMsNow
+  let graph <- parse nodeCount edgeList
+  let stop <- IO.monoMsNow
+  IO.println ("Read from file and converted to nat in: " ++ (toString (start - initial)) ++ " ms")
+  IO.println ("Parsed graph in: " ++ (toString (stop - start)) ++ " ms")
+  graph
 
---   IO.println (res)
-  
+def benchmarkTopSort (graph : Graph Bool Nat) : IO Unit := do
+  let start <- IO.monoMsNow
+  let res <- graph.topSortUnsafe
+  let stop <- IO.monoMsNow
+  IO.println ("Sorted graph in: " ++ (toString (stop - start)) ++ " ms")
+  evaluate res
+
+def benchmarkTopSortSafe (graph : Graph Bool Nat) : IO Unit := do
+  let start <- IO.monoMsNow
+  let res <- graph.topSort
+  let stop <- IO.monoMsNow
+  IO.println ("Safely sorted graph in: " ++ (toString (stop - start)) ++ " ms")
+  evaluate res
+
 
 def main (argv : List String) : IO Unit := do
 
   let filePath := huge
+  
+  let graph <- benchmarkParsing filePath
+  benchmarkTopSort graph
+  benchmarkTopSortSafe graph
 
-  let graph <- parseGraphFromEdgeListFile filePath
-  -- IO.println graph.vertices.back.payload
-  -- IO.println "Parsed graph"
-  let start <- IO.monoMsNow
-  -- let res <- graph.topSortUnsafe
-  let res <- graph.breadthFirstCompleteTraversalOrder
-  -- let res <- graph.topSort
-  let stop <- IO.monoMsNow
-  IO.println ("Sorted graph in: " ++ (toString (stop - start)) ++ " ms")
-  IO.println (res.back)
-  -- IO.println (res.get!.back)
+  
   
