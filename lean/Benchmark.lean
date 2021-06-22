2fa621102b296af4d8791f0617cd387744c62ad2
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-22 22:42:26 +0200

    Clean up Lean benchmarks

--- lean/Benchmark.lean
+++ lean/Benchmark.lean
@@ -2,23 +2,6 @@ import Graph.Graph
 import Graph.Parser
 import Graph.TopologicalSort
 
-
-def euMails := "../stanford-graphs/email-EuAll.txt"
-def gnutella8 := "../stanford-graphs/p2p-Gnutella08.txt"
-
-
-def bitcoinSize := "../generated-graphs/stanford-bitcoin-sized-topsort-gen.txt"
-def maximumSize := "../generated-graphs/maximum-working-size-topsort-gen.txt"
-def mediumDense := "../generated-graphs/medium-dense-topsort-gen.txt"
-def mediumVeryDense := "../generated-graphs/medium-very-dense-topsort-gen.txt"
-def mediumSparse := "../generated-graphs/medium-sparse-topsort-gen.txt"
-def mediumVerySparse := "../generated-graphs/medium-very-sparse-topsort-gen.txt"
-def smallDense := "../generated-graphs/small-dense-topsort-gen.txt"
-def smallSparse := "../generated-graphs/small-sparse-topsort-gen.txt"
-def huge := "../generated-graphs/huge-topsort-gen.txt"
-def testGraph := "../generated-graphs/test-topsort-gen.txt"
-
-
 /-- This function ensures that the expression does not get dropped by compiler optimizations. -/
 def evaluate [ToString α] (expression : α) : IO Unit := IO.FS.writeFile "/dev/null" (toString expression)
 
@@ -55,6 +38,3 @@ def main (argv : List String) : IO Unit := do
   let graph <- benchmarkParsing filePath
   benchmarkTopSort graph
   benchmarkTopSortSafe graph
-
-  
-  
