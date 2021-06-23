43aa1e6e36fd8fda0a396d07cfa7cd377e46238a
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-23 23:54:54 +0200

    Update Lean benchmark output

--- lean/Benchmark.lean
+++ lean/Benchmark.lean
@@ -12,22 +12,24 @@ def benchmarkParsing (filePath : String) : IO (Graph Bool Nat) := do
   let start <- IO.monoMsNow
   let graph <- parse nodeCount edgeList
   let stop <- IO.monoMsNow
-  IO.println ("Read from file and converted to nat in: " ++ (toString (start - initial)) ++ " ms")
-  IO.println ("Parsed graph in: " ++ (toString (stop - start)) ++ " ms")
+  -- IO.println ("Read from file and converted to nat in: " ++ (toString (start - initial)) ++ " ms")
+  -- IO.println ("Parsed graph in: " ++ (toString (stop - start)) ++ " ms")
   graph
 
 def benchmarkTopSort (graph : Graph Bool Nat) : IO Unit := do
   let start <- IO.monoMsNow
   let res <- graph.topSortUnsafe
   let stop <- IO.monoMsNow
-  IO.println ("Sorted graph in: " ++ (toString (stop - start)) ++ " ms")
+  IO.println ((toString (stop - start)) ++ " ms")
+  -- IO.println ("Sorted graph in: " ++ (toString (stop - start)) ++ " ms")
   evaluate res
 
 def benchmarkTopSortSafe (graph : Graph Bool Nat) : IO Unit := do
   let start <- IO.monoMsNow
   let res <- graph.topSort
   let stop <- IO.monoMsNow
-  IO.println ("Safely sorted graph in: " ++ (toString (stop - start)) ++ " ms")
+  IO.println ((toString (stop - start)) ++ " ms")
+  -- IO.println ("Safely sorted graph in: " ++ (toString (stop - start)) ++ " ms")
   evaluate res
 
 
